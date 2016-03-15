ac1cb2426a954397b3fe2d074ccc5fcae56c8188
CeeJay333 <carstenjunk@posteo.de>
2016-03-15 15:20:46 +0100

    Merge Sort von Wikipedia übernommen

--- src/algorithmen/MergeSort.java
+++ src/algorithmen/MergeSort.java
@@ -3,18 +3,72 @@ package algorithmen;
 public class MergeSort {
 	public long sortAsc(int A[], int p, int r){
 		Funktionen f = new Funktionen();
+		f.printArray(A);
 		f.setZeitStart();
-		mergeSort(A,p,r);
+		A = mergeSort(A);
+		//mergeSort(A,p,r);
 		f.setZeitStop();
+		f.printArray(A);
 		return f.getZeitUnterschied();
 	}
 	
+	private int[] mergeSort(int A[]){
+		int len = A.length;
+		if(len <=1){
+			return A;
+		}else{			
+			int q = len / 2;
+			int L[], R[];
+			L = new int[q];
+			R = new int[len-q];
+			for(int i=0;i<q;i++){
+				L[i] = A[i];
+			}
+			for(int i=q;i<len;i++){
+				R[i-q]= A[i];
+			}
+			L = mergeSort(L);
+			R = mergeSort(R);
+			return merge(L,R);
+		}
+	}
+	
+	private int[] merge(int L[], int R[]){
+		int B[], il,ir,ib;
+		B = new int[L.length+R.length];
+		il = 0;
+		ir = 0;
+		ib = 0;
+		while(il<L.length && ir<R.length){
+			if(L[il]<R[ir]){
+				B[ib] = L[il];
+				il++;
+				ib++;
+			}else{
+				B[ib] = R[ir];
+				ir++;
+				ib++;
+			}
+		}
+		while(il<L.length){
+			B[ib] = L[il];
+			ib++;
+			il++;
+		}
+		while(ir<R.length){
+			B[ib] = R[ir];
+			ir++;
+			ib++;
+		}		
+		return B;
+	}
+	/*
 	private int[] mergeSort(int A[],int p, int r){
 		int q;
 		if(p<r){
 			q = (p+r)/2;
-			A = mergeSort(A,p,q);
-			A = mergeSort(A,q+1,r);
+			mergeSort(A,p,q);
+			mergeSort(A,q+1,r);
 			A = merge(A,p,q,r);
 		}
 		return A;
@@ -50,5 +104,5 @@ public class MergeSort {
 			}
 		}
 		return A;
-	}
+	}*/
 }
